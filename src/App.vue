908c59fb024e4a278191dd49444f4cafbe4ce017
KimiArnul <2020908@clases.edu.sv>
2024-09-12 10:47:49 -0600

    Creación del menu de navegación en App.Vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,54 @@
 <template>
-  <nav>
-    <router-link to="/about">About</router-link> |
-    <router-link to="/">Productos</router-link>
-  </nav>
-  <router-view/>
+  <header>
+    <nav>
+      <div class="logo">
+        <router-link to="/">
+          <i class="fa-solid fa-shop"></i> Mall Lic
+        </router-link>
+      </div>
+      <ul class="menu">
+        <li><router-link to="/">Inicio</router-link></li>
+        <li><router-link to="/">Productos</router-link></li>
+        <li><router-link to="/">Categorias</router-link></li>
+        <li><router-link to="/">Ofertars</router-link></li>
+        <li><router-link to="/">Contacto</router-link></li>
+        <li><router-link to="/about">Acerda De</router-link></li>
+      </ul>
+    </nav>
+  </header>
+  <router-view />
 </template>
-<style scoped>
-  nav {
-    background-color: greenyellow;
-  }
-</style>
 
+<style scoped>
+
+header{
+  padding: 1.5rem;
+  background-color: #141B41;
+}
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.logo a{
+  font-family: 'Montserrat';
+  font-weight: 700;
+  font-size: 2rem;
+  color: #fff;
+  text-decoration: none;
+}
+
+.menu{
+  list-style: none;
+  font-family: 'Cabin';
+  font-size: 1.2rem;
+  display: flex;
+  gap: 40px;
+}
+
+.menu a {
+  color: #fff;
+  text-decoration: none;
+}
+</style>
