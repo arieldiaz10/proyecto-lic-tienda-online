4a1fd001d6974d89978aa907fdfa2fdbbaabce4f
arieldiazdev28 <arieldiaz.inac10@gmail.com>
2024-09-18 23:43:30 -0600

    Crea componentes LogoCard, HeaderCard y NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,81 +1,20 @@
 <template>
-  <header class="container-fluid">
-    <nav class="row d-flex">
-      <div class="logo col col-sm-4">
-        <router-link to="/">
-          <i class="fa-solid fa-shop"></i> MarketPlus
-        </router-link>
-      </div>
-      <ul class="menu col col-sm-8">
-        <li><router-link to="/productos">Productos</router-link></li>
-        <li><router-link to="/">Categorias</router-link></li>
-        <li><router-link to="/">Ofertas</router-link></li>
-        <li><router-link to="/about"><i class="fa-solid fa-cart-shopping"></i> Carrito</router-link></li>
-      </ul>
-    </nav>
-  </header>
+  <!--Sección del header-->
+  <HeaderCard></HeaderCard>
+  <!--Sección nav de la parte-->
+  <NavBarCard></NavBarCard>
+  <!--Etiqueta para el enrutador-->
   <router-view />
 </template>
 
-<style scoped>
-
-header{
-  padding: 1.2rem;
-  background-color: #141B41;
+<script>
+import NavBarCard from './components/NavBarCard.vue'
+import HeaderCard from './components/HeaderCard.vue'
+export default {
+  name: 'App',
+  components: {
+    NavBarCard,
+    HeaderCard
+  }
 }
-
-.row{
-  align-items: center;
-}
-
-.logo a{
-  font-family: 'Montserrat';
-  font-weight: 700;
-  font-size: 2rem;
-  color: #fff;
-  text-decoration: none;
-}
-
-.menu{
-  list-style: none;
-  font-family: 'Cabin';
-  font-size: 1.4rem;
-  display: flex;
-  justify-content: center;
-  gap: 50px;
-}
-
-.menu a {
-  color: #fff;
-  text-decoration: none;
-}
-
-/* Diseño responsivo */
-@media (min-width: 768px) and (max-width: 991px) {
-    .logo a{
-      font-size: 1.5rem;
-    }
-
-   .menu {
-    font-size: 1.2rem;
-   }
-}
-
-@media (min-width: 992px) and (max-width: 1199px) {
-    .col {
-        padding: 3.5rem;
-    }
-
-    .hero-text h1 {
-        font-size: 2.7rem;
-    }
-
-    .hero-texto p {
-        font-size: 1.2rem;
-    }
-
-    button {
-        padding: 1em;
-    }
-}
-</style>
+</script>
---
+++ src/components/LogoCard.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="logo">
+        <span>TotalMarket</span> <i class="bi bi-bag-heart-fill"></i>
+    </div>
+</template>
+
+<script>
+export default{
+    name: 'LogoCard'
+}
+</script>
+
+<style scoped>
+.logo {
+  font-size: 2rem;
+  color: #000000;
+}
+
+.logo span {
+  font-family: 'Montserrat';
+  font-weight: 800;
+  letter-spacing: 1.5px;
+}
+</style>
